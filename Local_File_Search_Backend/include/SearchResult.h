3a76e6f54f543bcb7612143aa141de633511d69f
Darius5273 <149438934+Darius5273@users.noreply.github.com>
2025-04-12 00:53:54 +0300

    feat(ScoreResult): add variable score and getters for the member fields

--- Local_File_Search_Backend/include/SearchResult.h
+++ Local_File_Search_Backend/include/SearchResult.h
@@ -7,9 +7,18 @@
 class SearchResult {
 public:
     std::string path;
+    double score;
     std::string previewText;
 
-    SearchResult(const std::string& path, const std::string& previewText);
+    SearchResult(const std::string& path, const double score, const std::string& previewText);
+
+    const std::string &getPath() const;
+
+    double getScore() const;
+
+    const std::string &getPreviewText() const;
+
+    void setScore(double score);
 };
 
 #endif // SEARCH_RESULT_H
--- Local_File_Search_Backend/src/SearchResult.cpp
+++ Local_File_Search_Backend/src/SearchResult.cpp
@@ -1,4 +1,20 @@
 #include "../include/SearchResult.h"
 
-SearchResult::SearchResult(const std::string& path, const std::string& previewText)
-        : path(path), previewText(previewText) {}
+SearchResult::SearchResult(const std::string& path, const double score, const std::string& previewText)
+        : path(path), score(score), previewText(previewText) {}
+
+const std::string &SearchResult::getPath() const {
+    return path;
+}
+
+double SearchResult::getScore() const {
+    return score;
+}
+
+void SearchResult::setScore(double score) {
+    SearchResult::score = score;
+}
+
+const std::string &SearchResult::getPreviewText() const {
+    return previewText;
+}
