1152575d0c4c1e1f9aa7f1208098b6168ef774cf
Darius5273 <149438934+Darius5273@users.noreply.github.com>
2025-04-12 17:01:56 +0300

    fix(indexscorer):resolve linker due to multiple definitions

--- Local_File_Search_Backend/include/IndexScorer.h
+++ Local_File_Search_Backend/include/IndexScorer.h
@@ -5,24 +5,6 @@
 #include <unordered_map>
 #include <unordered_set>
 
-const long long KB = 1024;
-const long long MB = 1024 * KB;
-const long long GB = 1024 * MB;
-std::unordered_set<std::string> textExtensions = {".txt", ".md", ".py", ".c", ".cpp", ".java"};
-std::unordered_map<std::string, double> mimeTypeScores = {
-        {"application/pdf", 0.9},
-        {"application/msword", 0.9},
-        {"application/vnd.openxmlformats-officedocument.wordprocessingml.document", 0.9},
-        {"application/vnd.ms-excel", 0.7},
-        {"application/octet-stream", 0.1},
-        {"image/png", 0.5},
-        {"image/jpeg", 0.5},
-        {"text/plain", 0.8},
-        {"text/x-c", 0.7},
-        {"text/x-c++", 0.7},
-        {"text/x-python", 0.7}
-};
-
 class IndexScorer {
 public:
 
--- Local_File_Search_Backend/src/IndexScorer.cpp
+++ Local_File_Search_Backend/src/IndexScorer.cpp
@@ -4,6 +4,24 @@
 #include <algorithm>
 #include <iomanip>
 
+const long long KB = 1024;
+const long long MB = 1024 * KB;
+const long long GB = 1024 * MB;
+std::unordered_set<std::string> textExtensions = {".txt", ".md", ".py", ".c", ".cpp", ".java"};
+std::unordered_map<std::string, double> mimeTypeScores = {
+        {"application/pdf", 0.9},
+        {"application/msword", 0.9},
+        {"application/vnd.openxmlformats-officedocument.wordprocessingml.document", 0.9},
+        {"application/vnd.ms-excel", 0.7},
+        {"application/octet-stream", 0.1},
+        {"image/png", 0.5},
+        {"image/jpeg", 0.5},
+        {"text/plain", 0.8},
+        {"text/x-c", 0.7},
+        {"text/x-c++", 0.7},
+        {"text/x-python", 0.7}
+};
+
 double IndexScorer::scoreFile(const std::string& path, const std::string& extension,
                          const std::string& mime_type, const std::string& modified_time,
                          long long size, bool is_text)
