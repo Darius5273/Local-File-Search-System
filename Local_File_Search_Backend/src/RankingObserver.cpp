8da3249ec784cfe80b1ce5ae0ddc3913305c5396
Darius5273 <149438934+Darius5273@users.noreply.github.com>
2025-04-12 20:53:15 +0300

    fix(RankingObserver): modify to return the greatest ranked files

--- Local_File_Search_Backend/src/RankingObserver.cpp
+++ Local_File_Search_Backend/src/RankingObserver.cpp
@@ -2,41 +2,42 @@
 #include "../include/RankingObserver.h"
 #include <algorithm>
 #include <queue>
+#include <iostream>
 
 RankingObserver::RankingObserver(DatabaseConnector* db) : db(db) {}
 
 struct CompareSearchResultByScore {
     bool operator() (const SearchResult& a, const SearchResult& b) {
-        return a.getScore() < b.getScore();
+        return a.getScore() > b.getScore();
     }
 };
 
 void RankingObserver::update(const std::unordered_map<std::string, std::vector<std::string>>& parsedQuery) {
     auto results = db->query(parsedQuery);
     for (auto& result : results) {
-        double score = rankingMap[result.path].score;
+        double score = rankingMap[result.getPath()].score;
         double newResult = result.getScore() + score;
         result.setScore(newResult);
     }
 
-    std::priority_queue<SearchResult, std::vector<SearchResult>, CompareSearchResultByScore> maxHeap;
+    std::priority_queue<SearchResult, std::vector<SearchResult>, CompareSearchResultByScore> minHeap;
     for (const auto& result : results) {
-        maxHeap.push(result);
-        if (maxHeap.size() > 3) {
-            maxHeap.pop();
+        minHeap.push(result);
+        if (minHeap.size() > 3) {
+            minHeap.pop();
         }
     }
     std::vector<std::string> historyEntry;
     rankingResults.clear();
-    for (int i = 0; i < maxHeap.size(); ++i) {
-        auto result = maxHeap.top();
+    for (int i = 0; i < 3; ++i) {
+        auto result = minHeap.top();
         std::string path = result.getPath();
-        maxHeap.pop();
+        minHeap.pop();
         rankingResults.push_back(result);
         auto &info = rankingMap[path];
-        if (i == 1) info.first++;
-        else if (i == 2) info.second++;
-        else if (i == 3) info.third++;
+        if (i == 0) info.third++;
+        else if (i == 1) info.second++;
+        else if (i == 2) info.first++;
 
         info.score = (1.0 * info.first + 0.5 * info.second + 0.2 * info.third) / 100.0;
         historyEntry.push_back(path);
@@ -52,9 +53,9 @@ void RankingObserver::maintainRecentResults() {
 
         for (int i = 0; i < oldResults.size(); ++i) {
             auto &info = rankingMap[oldResults[i]];
-            if (i == 0) info.first--;
+            if (i == 0) info.third--;
             else if (i == 1) info.second--;
-            else if (i == 2) info.third--;
+            else if (i == 2) info.first--;
 
             if (info.first == 0 && info.second == 0 && info.third == 0) {
                 rankingMap.erase(oldResults[i]);
