0836b732e25b8b556bc00eebdb7abea86c2b8158
Darius5273 <149438934+Darius5273@users.noreply.github.com>
2025-05-19 23:58:00 +0300

    feat: modify interfaces to include metadata

---
+++ Web Application/src/interfaces/MetadataBucket.tsx
@@ -0,0 +1,3 @@
+export interface MetadataBucket {
+  [key: string]: string | number;
+}
--- Web Application/src/interfaces/SearchResponse.tsx
+++ Web Application/src/interfaces/SearchResponse.tsx
@@ -4,5 +4,15 @@ import { SearchResult } from "./SearchResult";
 export interface SearchResponse {
     suggestions: string[];
     rankingResults: SearchResult[];
+    correctedQuery: string;
+    metadataSummary: {
+    totalFiles: number;
+    totalSizeBytes: number;
+    fileTypeCategories: MetadataBucket[];
+    extensionCounts: MetadataBucket[];
+    yearCounts: MetadataBucket[];
+    sizeBuckets: MetadataBucket[];
+    languages?: MetadataBucket[];
+  };
   }
   
