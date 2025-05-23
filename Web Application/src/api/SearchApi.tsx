beaa0d851a5cbee6e69420b6fb1bc6e73052acca
Darius5273 <149438934+Darius5273@users.noreply.github.com>
2025-05-23 23:02:41 +0300

    style: delete comments

--- Web Application/src/api/SearchApi.tsx
+++ Web Application/src/api/SearchApi.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import { SearchRequest } from "../interfaces/SearchRequest";
-import { SearchResponse } from "../interfaces/SearchResponse";
+import { SearchResponse } from '../interfaces/SearchResponse';
+
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:8080',
@@ -10,7 +10,7 @@ const axiosInstance = axios.create({
 
 export async function searchFiles(query: string): Promise<SearchResponse> {
   try {
-    const response = await axiosInstance.post('/search', { query } as SearchRequest, {
+    const response = await axiosInstance.post('/search', { query }, {
       headers: {
         'Content-Type': 'application/json',
       },
@@ -19,11 +19,9 @@ export async function searchFiles(query: string): Promise<SearchResponse> {
     return response.data as SearchResponse;
   } catch (error) {
     if (axios.isAxiosError(error) && error.response) {
-      // Handle error returned from the server
       const errorMessage = error.response.data.error || 'Search request failed';
       throw new Error(errorMessage);
     } else {
-      // Handle general errors (network issues, timeout, etc.)
       throw new Error('Search request failed');
     }
   }
